0252b9ecbdd0c803171b8c0510d784b479ceb0e3
Roeland Strijk <roeland.strijk@gmail.com>
2025-05-14 10:16:47 +0200

    view aanpassingen

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$&yzvqnz-xv8f2a4bg!ojp6f9f3%^wwq^zz*)3z1&y2b1-vy$k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.2.169']
 
 
 # Application definition
