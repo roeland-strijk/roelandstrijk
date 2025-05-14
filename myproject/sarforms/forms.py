2bbea6280009b5907b917c42f2e9f8eb57c155b2
Roeland Strijk <roeland.strijk@gmail.com>
2025-05-14 16:42:54 +0200

    html aanpassing

--- myproject/sarforms/forms.py
+++ myproject/sarforms/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class Form133(forms.Form):
-    incident_nr=forms.IntegerField(null=True, blank=True)
+    incident_nr=forms.CharField(max_length=5)
     incident_naam=forms.CharField(max_length=30)
     datum=forms.DateField()
     locatie=forms.CharField(max_length=100)
